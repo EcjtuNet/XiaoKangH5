f4afb1f3d131303e1a6578bd8b7228742c337f2d
lsc95 <602650513@qq.com>
2016-08-28 17:45:08 +0800

    修改配置错误

---
+++ index.php
@@ -0,0 +1,78 @@
+<?php
+require_once "JsSdk.php";
+$jssdk = new JSSDK("wx81dffb5d17695838", "651936e8e2d358460475fc322d003965");
+$signPackage = $jssdk->GetSignPackage();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title></title>
+</head>
+<script src="http://res.wx.qq.com/open/js/jweixin-1.0.0.js"></script>
+<script>
+    // 注意：所有的JS接口只能在公众号绑定的域名下调用，公众号开发者需要先登录微信公众平台进入“公众号设置”的“功能设置”里填写“JS接口安全域名”。
+    // 如果发现在 Android 不能分享自定义内容，请到官网下载最新的包覆盖安装，Android 自定义分享接口需升级至 6.0.2.58 版本及以上。
+    // 完整 JS-SDK 文档地址：http://mp.weixin.qq.com/wiki/7/aaa137b55fb2e0456bf8dd9148dd613f.html
+    wx.config({
+        appId: '<?php echo $signPackage["appId"]; ?>',
+        timestamp:<?php echo $signPackage["timestamp"]; ?>,
+        nonceStr: '<?php echo $signPackage["nonceStr"]; ?>',
+        signature: '<?php echo $signPackage["signature"]; ?>',
+        jsApiList: [
+            // 所有要调用的 API 都要加到这个列表中
+            'onMenuShareTimeline',
+            'onMenuShareAppMessage',
+            'chooseImage'
+        ]
+    });
+    wx.ready(function () {
+        // config信息验证后会执行ready方法，所有接口调用都必须在config接口获得结果之后，config是一个客户端的异步操作，所以如果需要在页面加载时就调用相关接口，则须把相关接口放在ready函数中调用来确保正确执行。对于用户触发时才调用的接口，则可以直接调用，不需要放在ready函数中。
+        wx.onMenuShareTimeline({
+            title: '分享盆友圈', // 分享标题
+            link: 'http://www.baidu.com', // 分享链接
+            imgUrl: 'http://www.microfotos.com/pic/0/90/9023/902372preview4.jpg', // 分享图标
+            success: function () {
+                // 用户确认分享后执行的回调函数
+                alert("分享成功");
+            },
+            cancel: function () {
+                // 用户取消分享后执行的回调函数
+            }
+        });
+        wx.onMenuShareAppMessage({
+            title: '分享好友', // 分享标题
+            desc: 'test imooc', // 分享描述
+            link: 'http://www.baidu.com', // 分享链接
+            imgUrl: 'http://www.microfotos.com/pic/0/90/9023/902372preview4.jpg', // 分享图标
+            type: 'link', // 分享类型,music、video或link，不填默认为link
+            dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
+            success: function () {
+                // 用户确认分享后执行的回调函数
+            },
+            cancel: function () {
+                // 用户取消分享后执行的回调函数
+            }
+        });
+    });
+    function show(){
+        wx.chooseImage({
+            count: 1, // 默认9
+            sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
+            sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
+            success: function (res) {
+                var localIds = res.localIds; // 返回选定照片的本地ID列表，localId可以作为img标签的src属性显示图片
+            }
+        });
+    }
+    wx.error(function (res) {
+        // config信息验证失败会执行error函数，如签名过期导致验证失败，具体错误信息可以打开config的debug模式查看，也可以在返回的res参数中查看，对于SPA可以在这里更新签名。
+    });
+</script>
+<body>
+mdzz
+<button onclick="show()" >照片</button>
+<img src="#" id="show"/>
+</body>
+
+</html>
